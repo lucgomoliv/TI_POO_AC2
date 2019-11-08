ad9062803e6a05182ab0d50f206ec845b877bb17
Lucas Gomes Oliveira <lucasgomoliveira@gmail.com>
2019-11-08 17:17:58 -0300

    Adicionado subtrator

---
+++ TI_POO_AC2/Substrator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TI_POO_AC2
+{
+    public class Substrator : Circuito
+    {
+        public bool Operar(bool a, bool b, bool vemUm, out bool vaiUm, bool controle)
+        {
+            vaiUm = or.Operar(
+                            and.Operar(new bool[] { a, !b, controle }),
+                            and.Operar(new bool[] { controle, !xor.Operar(a, b), vemUm })
+                        );
+            return xor.Operar(vemUm, xor.Operar(a, b));
+        }
+    }
+}
---
+++ TI_POO_AC2/ULA8BITS.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TI_POO_AC2
+{
+    class ULA8BITS
+    {
+
+    }
+}
